221198d4e637d792885077552c86299a56a38426
jiaao <jiaao.liu@student.kuleuven.be>
2024-06-06 16:55:15 +0200

    define the flight Service

--- src/main/java/be/kuleuven/dsgt4/planeTicketRestService/PlaneTicketRestServiceApplication.java
+++ src/main/java/be/kuleuven/dsgt4/planeTicketRestService/PlaneTicketRestServiceApplication.java
@@ -1,13 +1,13 @@
-package be.kuleuven.dsgt4.hotelRestService;
+package be.kuleuven.dsgt4.planeTicketRestService;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.PropertySource;
 
 @SpringBootApplication
-@PropertySource("classpath:hotel-application.properties")
-public class HotelRestServiceApplication {
+@PropertySource("classpath:plane-application.properties")
+public class PlaneTicketRestServiceApplication {
     public static void main(String[] args) {
-        SpringApplication.run(HotelRestServiceApplication.class, args);
+        SpringApplication.run(PlaneTicketRestServiceApplication.class, args);
     }
 }
