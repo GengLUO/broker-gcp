55d90bde268413a817e39ef3dd00a9fc9d375a30
Jeffee Hsiung <j22132235@icloud.com>
2024-06-11 16:30:16 +0200

    added pub/sub classes, pending to be integrated with firestore

--- src/main/java/be/kuleuven/dsgt4/FirestoreController.java
+++ src/main/java/be/kuleuven/dsgt4/FirestoreController.java
@@ -30,11 +30,6 @@ class FirestoreController {
     public User whoami() throws InterruptedException, ExecutionException {
         var user = WebSecurityConfig.getUser();
         if (!user.isManager()) throw new AuthorizationServiceException("You are not a manager");
-
-        UUID buuid = UUID.randomUUID();
-        UserMessage b = new UserMessage(buuid, LocalDateTime.now(), user.getRole(), user.getEmail());
-        this.firestore.collection("usermessages").document(b.getId().toString()).set(b.toDoc()).get();
-
         return user;
     }
 
