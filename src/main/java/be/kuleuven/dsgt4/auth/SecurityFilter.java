1116cd08069b9ce47f40318cc3e13ca4839aae34
geng <grant.luogeng@gmail.com>
2024-06-12 22:33:19 +0200

    Do not use the admin sdk

--- src/main/java/be/kuleuven/dsgt4/auth/SecurityFilter.java
+++ src/main/java/be/kuleuven/dsgt4/auth/SecurityFilter.java
@@ -1,8 +1,15 @@
 package be.kuleuven.dsgt4.auth;
 
 import be.kuleuven.dsgt4.broker.domain.User;
+import com.auth0.jwk.Jwk;
+import com.auth0.jwk.JwkProvider;
 import com.auth0.jwt.JWT;
+import com.auth0.jwt.algorithms.Algorithm;
+import com.auth0.jwt.exceptions.JWTVerificationException;
 import com.auth0.jwt.interfaces.DecodedJWT;
+import com.auth0.jwt.interfaces.JWTVerifier;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.google.gson.*;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
@@ -16,9 +23,8 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.List;
+import java.security.interfaces.RSAPublicKey;
+import java.util.*;
 
 @Component
 public class SecurityFilter extends OncePerRequestFilter {
@@ -31,16 +37,39 @@ public class SecurityFilter extends OncePerRequestFilter {
             String token = authorizationHeader.substring(7); // Remove "Bearer " prefix
             System.out.println("Authorization Header: " + authorizationHeader);
             System.out.println("Extracted Token: " + token);
-// (level 1) decode Identity Token and assign correct email and role
-            DecodedJWT jwt = JWT.decode(token); // Decode without verification
+            try {
+                JwkProvider provider = new JwkConfiguration().jwkProvider();
+                DecodedJWT jwt = JWT.decode(token);
+                Jwk jwk = provider.get(jwt.getKeyId());
 
-            String email = jwt.getClaim("email").asString();
+                Algorithm algorithm = Algorithm.RSA256((RSAPublicKey) jwk.getPublicKey(), null);
+                JWTVerifier verifier = JWT.require(algorithm)
+                        .withIssuer("https://securetoken.google.com/broker-da44b")
+                        .build(); // Reusable verifier instance
+                DecodedJWT verifiedJwt = verifier.verify(token);
+                System.out.println("JWT Verified. Subject: " + verifiedJwt.getSubject());
+
+                String email = jwt.getClaim("email").asString();
 //          TODO: handle users with multiple roles
-            List<String> roles = jwt.getClaim("roles").asList(String.class);
-            String role = roles != null && !roles.isEmpty() ? roles.get(0) : "user" ;
-            User user = new User(email, role);
-            SecurityContext context = SecurityContextHolder.getContext();
-            context.setAuthentication(new FirebaseAuthentication(user));
+                List<String> roles = jwt.getClaim("roles").asList(String.class);
+                String role = roles != null && !roles.isEmpty() ? roles.get(0) : "user" ;
+                User user = new User(email, role);
+                SecurityContext context = SecurityContextHolder.getContext();
+                context.setAuthentication(new FirebaseAuthentication(user));
+            } catch (Exception e) {
+                System.err.println("Failed to verify token: " + e.getMessage());
+                e.printStackTrace();
+            }
+// (level 1) decode Identity Token and assign correct email and role
+//            DecodedJWT jwt = JWT.decode(token); // Decode without verification
+//
+//            String email = jwt.getClaim("email").asString();
+////          TODO: handle users with multiple roles
+//            List<String> roles = jwt.getClaim("roles").asList(String.class);
+//            String role = roles != null && !roles.isEmpty() ? roles.get(0) : "user" ;
+//            User user = new User(email, role);
+//            SecurityContext context = SecurityContextHolder.getContext();
+//            context.setAuthentication(new FirebaseAuthentication(user));
 
 //(level 2) verify Identity Token
 //            try {
--- src/main/java/be/kuleuven/dsgt4/auth/TokenVerifier.java
+++ src/main/java/be/kuleuven/dsgt4/auth/TokenVerifier.java
@@ -2,29 +2,68 @@
 //
 //import com.auth0.jwk.JwkProvider;
 //import com.auth0.jwt.JWT;
+//import com.auth0.jwt.exceptions.JWTVerificationException;
 //import com.auth0.jwt.interfaces.DecodedJWT;
 //import com.auth0.jwt.interfaces.JWTVerifier;
 //import com.auth0.jwt.algorithms.Algorithm;
 //import com.auth0.jwk.Jwk;
+//import com.google.gson.Gson;
+//
+//import java.net.URI;
+//import java.net.http.HttpClient;
+//import java.net.http.HttpRequest;
+//import java.net.http.HttpResponse;
+//import java.security.KeyFactory;
 //import java.security.interfaces.RSAPublicKey;
+//import java.security.spec.X509EncodedKeySpec;
+//import java.util.Base64;
+//import java.util.Map;
 //
 //public class TokenVerifier {
 //
-//    private final JwkProvider jwkProvider;
+//    private static final String GOOGLE_PUBLIC_KEYS_URL = "https://www.googleapis.com/oauth2/v3/certs";
 //
-//    public TokenVerifier(JwkProvider jwkProvider) {
-//        this.jwkProvider = jwkProvider;
-//    }
-//
-//    public DecodedJWT verifyToken(String token) throws Exception {
-//        DecodedJWT jwt = JWT.decode(token);
-//        Jwk jwk = jwkProvider.get(jwt.getKeyId());
-//
-//        Algorithm algorithm = Algorithm.RSA256((RSAPublicKey) jwk.getPublicKey(), null);
-//        JWTVerifier verifier = JWT.require(algorithm)
-//                .withIssuer("https://securetoken.google.com/demo-distributed-systems-kul")
+//    private static Map<String, String> getGooglePublicKeys() throws Exception {
+//        HttpClient client = HttpClient.newHttpClient();
+//        HttpRequest request = HttpRequest.newBuilder()
+//                .uri(new URI(GOOGLE_PUBLIC_KEYS_URL))
 //                .build();
-//
-//        return verifier.verify(token);
+//        HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+//        return new Gson().fromJson(response.body(), Map.class);
 //    }
+//
+//    private static RSAPublicKey extractPublicKey(String publicKeyStr) throws Exception {
+//        String publicKeyPEM = publicKeyStr.replace("-----BEGIN PUBLIC KEY-----", "")
+//                .replaceAll(System.lineSeparator(), "")
+//                .replace("-----END PUBLIC KEY-----", "");
+//
+//        byte[] encoded = Base64.getDecoder().decode(publicKeyPEM);
+//        X509EncodedKeySpec keySpec = new X509EncodedKeySpec(encoded);
+//        KeyFactory keyFactory = KeyFactory.getInstance("RSA");
+//        return (RSAPublicKey) keyFactory.generatePublic(keySpec);
+//    }
+//
+//    private static DecodedJWT verifyToken(String token, RSAPublicKey publicKey) throws Exception {
+//        try {
+//            Algorithm algorithm = Algorithm.RSA256(publicKey, null);
+//            JWTVerifier verifier = JWT.require(algorithm)
+//                    .withIssuer("https://securetoken.google.com/broker-da44b")
+//                    .build(); // Reusable verifier instance
+//            return verifier.verify(token);
+//        } catch (JWTVerificationException exception) {
+//            throw new Exception("Token verification failed.", exception);
+//        }
+//    }
+//
+////    public DecodedJWT verifyToken(String token) throws Exception {
+////        DecodedJWT jwt = JWT.decode(token);
+////        Jwk jwk = jwkProvider.get(jwt.getKeyId());
+////
+////        Algorithm algorithm = Algorithm.RSA256((RSAPublicKey) jwk.getPublicKey(), null);
+////        JWTVerifier verifier = JWT.require(algorithm)
+////                .withIssuer("https://securetoken.google.com/demo-distributed-systems-kul")
+////                .build();
+////
+////        return verifier.verify(token);
+////    }
 //}
