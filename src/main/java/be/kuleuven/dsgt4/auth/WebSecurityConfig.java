bfac66fe6ed61154daa71b3e4fd029a66fa0d967
geng <grant.luogeng@gmail.com>
2024-06-13 14:35:53 +0200

    Use annotation to enable the role check

--- src/main/java/be/kuleuven/dsgt4/auth/WebSecurityConfig.java
+++ src/main/java/be/kuleuven/dsgt4/auth/WebSecurityConfig.java
@@ -1,6 +1,7 @@
 package be.kuleuven.dsgt4.auth;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -12,6 +13,7 @@ import be.kuleuven.dsgt4.broker.domain.User;
 
 @Configuration
 @EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true)
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
     private final SecurityFilter securityFilter;
