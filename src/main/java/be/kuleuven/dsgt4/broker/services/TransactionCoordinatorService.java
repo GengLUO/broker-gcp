1bd907ca8cc310bef6bfaa3cdfb43674bf60c5fa
jiaao <jiaao.liu@student.kuleuven.be>
2024-06-15 23:38:30 +0200

    debug confirm passengers button in cloud deployment

--- src/main/java/be/kuleuven/dsgt4/broker/services/TransactionCoordinatorService.java
+++ src/main/java/be/kuleuven/dsgt4/broker/services/TransactionCoordinatorService.java
@@ -175,11 +175,11 @@ public class TransactionCoordinatorService {
     // Methods for Before Booking
     public void addFlightToPackage(String userId, String packageId, Map<String, Object> flightDetails) throws ExecutionException, InterruptedException {
         Firestore db = firestore;
-//        db.runTransaction(transaction -> {
+        db.runTransaction(transaction -> {
 //            DocumentReference packageRef = db.collection("travelPackages").document(packageId);
 //            transaction.update(packageRef, "flights", FieldValue.arrayUnion(flightDetails));
-//               return null;
-//        }).get();
+               return null;
+        }).get();
     }
 
     public void removeFlightFromPackage(String userId, String packageId, String flightId) throws ExecutionException, InterruptedException {
