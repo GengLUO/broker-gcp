dbc1726ded956acd9b83dad07b9ad4c4cc321dfa
geng <grant.luogeng@gmail.com>
2024-06-15 23:01:31 +0200

    add webClientBuilder

--- src/main/java/be/kuleuven/dsgt4/flightRestService/FlightRestServiceApplication.java
+++ src/main/java/be/kuleuven/dsgt4/flightRestService/FlightRestServiceApplication.java
@@ -1,22 +1,31 @@
 package be.kuleuven.dsgt4.flightRestService;
 
-import be.kuleuven.dsgt4.flightRestService.services.FlightService;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.PropertySource;
+import org.springframework.hateoas.config.EnableHypermediaSupport;
+import org.springframework.hateoas.config.HypermediaWebClientConfigurer;
+import org.springframework.http.client.reactive.ReactorClientHttpConnector;
+import org.springframework.web.reactive.function.client.WebClient;
+import reactor.netty.http.client.HttpClient;
 
+@EnableHypermediaSupport(type = EnableHypermediaSupport.HypermediaType.HAL)
 @SpringBootApplication
 @PropertySource("classpath:plane-application.properties")
 public class FlightRestServiceApplication {
     public static void main(String[] args) {
         SpringApplication.run(FlightRestServiceApplication.class, args);
+    }
 
-        FlightService flightService = new FlightService();
-        try {
-            flightService.startSubscriber();
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
+    /*
+     * You can use this builder to create a Spring WebClient instance which can be used to make REST-calls.
+     */
+    @Bean
+    WebClient.Builder webClientBuilder(HypermediaWebClientConfigurer configurer) {
+        return configurer.registerHypermediaTypes(WebClient.builder()
+                .clientConnector(new ReactorClientHttpConnector(HttpClient.create()))
+                .codecs(clientCodecConfigurer -> clientCodecConfigurer.defaultCodecs().maxInMemorySize(100 * 1024 * 1024)));
     }
 }
 
